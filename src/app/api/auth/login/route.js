72dae995ceb6d523d69fec461a496f54256f015d
Gorasiyavaidik <gorasiyavaidik@gmail.com>
2024-07-18 18:40:53 +0530

    auth added

---
+++ src/app/api/auth/login/route.js
@@ -0,0 +1,58 @@
+import { dbConnect } from '@/lib/db';
+import Tester from '@/model/testerModel';
+import Creator from '@/model/creatorModel';
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+dbConnect();
+
+const JWT_SECRET = "your_jwt_secret_key"; // Replace this with your actual secret key, preferably stored in environment variables
+
+export async function POST(req) {
+    try {
+        const reqBody = await req.json();
+        const { email, password, role } = reqBody;
+
+        const missingFields = [];
+        if (!email) missingFields.push('email');
+        if (!password) missingFields.push('password');
+        if (!role) missingFields.push('role');
+        if (missingFields.length > 0) {
+            return NextResponse.json({ message: "The following fields are required: " + missingFields.join(', ') });
+        }
+        if (role !== "tester" && role !== "creator") {
+            return NextResponse.json({ message: "Invalid Role" });
+        }
+
+        let existingUser;
+        if (role === "tester") {
+            existingUser = await Tester.findOne({ email });
+            if (!existingUser) {
+                return NextResponse.json({ message: "Tester not found" });
+            }
+        } else {
+            existingUser = await Creator.findOne({ email });
+            if (!existingUser) {
+                return NextResponse.json({ message: "Creator not found" });
+            }
+        }
+
+        const isMatch = await bcryptjs.compare(password, existingUser.password);
+        if (!isMatch) {
+            return NextResponse.json({ message: "Invalid credentials" });
+        }
+
+        // Create a JWT token
+        const token = jwt.sign(
+            { id: existingUser._id, email: existingUser.email, role: existingUser.role },
+            JWT_SECRET,
+            { expiresIn: '24h' } // Token expires in 24 hour
+        );
+
+        return NextResponse.json({ message: `${role.charAt(0).toUpperCase() + role.slice(1)} logged in successfully`, token });
+    } catch (error) {
+        console.error('Error:', error);
+        return NextResponse.json({ message: "An error occurred", error: error.message });
+    }
+}
---
+++ src/model/Task/youtubetaskModel.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const youtubetaskSchema = new mongoose.Schema({
+    creator :{
+        type : Schema.Types.ObjectId,
+        ref : 'Creator',
+        required : true
+    },
+    post_date : {
+        type : Date,
+        default : Date.now,
+        required : true
+    },
+    end_date: {
+        type : Date,
+        required : true
+    },
+    tester_no : {
+        type : Number,
+        required : true
+    },
+    tester_age: {
+        type : Number,
+        required : true
+    },
+    tester_gender:{
+        type : String,
+        required : true
+    },
+    country:{
+        type : String,
+        required : true
+    },
+    heading:{
+        type : String,
+        required : true
+    },
+    instruction:{
+        type : String,
+        required : true
+    },
+    youtube_thumbnails:[{
+        title:String,
+    }],
+    web_link:{
+        type : String,
+        required : true
+    },
+    tester_ids : [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Tester'
+        }
+    ],
+})
+
+const Youtube = mongoose.models.youtube || mongoose.model("youtube", youtubetaskSchema);
+
+export default Youtube;
